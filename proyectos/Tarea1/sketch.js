5e33cbae41557009341fdc8c14fc77f001275547
Miloppp <miloo.v4nt3@gmail.com>
2024-08-29 14:44:00 -0400

    Initial commit

---
+++ proyectos/Tarea1/sketch.js
@@ -0,0 +1,104 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //fondo
+  background(220);
+  background (89, 111, 162)
+  //cuadrado grande
+  fill (173, 190, 228)
+  strokeWeight (0)
+  rect (10,10,40,40)
+  //cuadrado chico
+  fill (3, 45, 144)
+  strokeWeight (0)
+  rect (17,17,25,25)
+  //cuadrado más chico
+  fill (225,225,225)
+  rect (24,24,10,10)
+  //cuadraddo grande 2
+  fill(3, 45, 144)
+  rect (60,10,40,40)
+  //cuadrado chico 2
+  fill(225,225,225)
+  rect (67.5,17,25,25)
+  //cuadrado más chico 2
+  fill(173,190,228)
+  rect (75,24,10,10)
+  //cuadrado grande 3
+  fill (225,225,225)
+  rect (10,60,40,40)
+  //cuadrado chico 3
+  fill(173,190,228)
+  rect (17,67.5,25,25)
+  //cuadrado más chico 3
+  fill(3,45,144)
+  rect (24,75,10,10)
+  //cuadrado grande 4
+  fill(173,190,228)
+  rect (60,60,40,40)
+  //cuadrdao chico 4
+  fill (3,45,144)
+  rect (67.5,67.5,25,25)
+  //cuadrdado más chico 4
+  fill(225,225,225)
+  rect(75,75,10,10)
+  
+fill(225,225,225)
+rect (10,110,90,90)
+
+  fill (4,45,144)
+  rect(20,120,70,70)
+ 
+ fill(173,190,228)
+  rect(35,135,40,40)
+  
+  fill(4,45,144)
+  rect(110,10,90,90)
+ 
+   fill (173,190,228)
+  rect (120,20,70,70)
+  
+  fill (225,225,225)
+  rect (134,35,40,40)
+  
+  fill (173,190,228)
+  rect (110,110,90,90)
+  
+  fill (225,225,225)
+  rect (120,120,70,70)
+  
+  fill (4,45,144)
+  rect (135,134,40,40)
+  
+  fill (4,45,144)
+  rect (210,10,180,190)
+  
+fill (225,225,225)
+  rect (225,25,150,160)
+  
+  fill(173,190,228)
+  rect (240,40,120,130)
+  
+  fill (172,190,228)
+  rect (10,210,190,180)
+  
+  fill (4,45,144)
+  rect (25,225,160,150)
+  
+  fill (225,225,225)
+  rect (40,240,130,120)
+  
+  fill (225,225,225)
+  rect (210,210,180,180)
+  
+  fill(173,190,228)
+  rect (225,225,150,150)
+  
+  fill (4,45,144)
+  rect (240,240,120,120)
+  
+  fill ()
+  ellipse (222,170,200)
+}
